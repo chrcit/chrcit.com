893f55205faf6935bf0b1fd7542a205628f0720e
Christian Cito <citochris@gmail.com>
2024-01-03 13:36:13 +0100

    small fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        ssr: {
-            external: ['twitter-api-client']
-        }
-    },
-    site: 'https://chrcit.com',
-    integrations: [tailwind(), sitemap(), mdx()]
+  vite: {
+    ssr: {
+      external: ['twitter-api-client']
+    }
+  },
+  site: 'https://chrcit.com',
+  integrations: [tailwind(), sitemap(), mdx()]
 });
