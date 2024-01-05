6d0c196ce75000e5298256db5695fc681f3f2b4e
Christian Cito <citochris@gmail.com>
2024-01-05 11:00:12 +0100

    add prettier format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,30 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import sitemap from '@astrojs/sitemap';
-import mdx from '@astrojs/mdx';
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 import partytown from "@astrojs/partytown";
 import rehypeSlug from "rehype-slug";
-import remarkSectionize from 'remark-sectionize'
+import remarkSectionize from "remark-sectionize";
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     ssr: {
-      external: ['twitter-api-client']
-    }
+      external: ["twitter-api-client"],
+    },
   },
   markdown: {
-    remarkPlugins: [
-      remarkSectionize,
-    ],
+    remarkPlugins: [remarkSectionize],
     rehypePlugins: [
-      [rehypeSlug, {
-        prefix: 'heading-'
-      }]
-    ]
+      [
+        rehypeSlug,
+        {
+          prefix: "heading-",
+        },
+      ],
+    ],
   },
-  site: 'https://chrcit.com',
-  integrations: [tailwind(), sitemap(), mdx(), robotsTxt()]
+  site: "https://chrcit.com",
+  integrations: [tailwind(), sitemap(), mdx(), robotsTxt()],
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,96 +1,95 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				brand: "#8B1717"
-			},
-			animation: {
-				'fade-in-up': 'fade-in-up 0.3s ease-in forwards',
-				'fade-in': 'fade-in 0.3s ease-in forwards',
-			},
-			screens: {
-				'notouch': { 'raw': '(hover: hover)' },
-				"2xl": "1440px",
-			},
-			keyframes: {
-				'fade-in-up': {
-					'0%': {
-						opacity: '0',
-						transform: 'translateY(5rem)'
-					},
-					'100%': {
-						opacity: '1',
-						transform: 'translateY(0)'
-					},
-				},
-				'fade-in': {
-					'0%': {
-						opacity: '0',
-					},
-					'100%': {
-						opacity: '1',
-					},
-				},
-			},
-			fontFamily: {
-				sans: ["Monterrat", "sans-serif"],
-				serif: ["Playfair Display", "serif"],
-			},
-			typography: (theme) => ({
-				DEFAULT: {
-					css: {
-						'--tw-prose-links': theme('colors.brand'),
-						fontFamily: theme('fontFamily.sans').join(','),
-						color: theme('colors.gray.700'),
-						maxWidth: '100%',
-						width: '70ch',
-						"a": {
-							textDecoration: 'none',
-							underlineOffset: '0.5em',
-							color: theme('colors.brand'),
-							'&:hover': {
-								textDecoration: 'underline',
-							},
-						},
-						"h1,h2,h3,h4": {
-							fontFamily: theme('fontFamily.serif').join(','),
-							fontWeight: '600',
-						},
-						"h1": {
-							fontSize: theme('fontSize.4xl'),
-						},
-						"h2": {
-							fontSize: theme('fontSize.4xl'),
-							marginTop: theme('spacing.10'),
-							marginBottom: theme('spacing.3'),
-						},
-						"h3": {
-							fontSize: theme('fontSize.2xl'),
-						},
-						"strong": {
-							fontWeight: '600',
-						}
-					},
-				},
-				'lg': {
-					css: {
-						"h1": {
-						},
-						"h2": {
-							fontSize: theme('fontSize.5xl'),
-							marginTop: theme('spacing.10'),
-							marginBottom: theme('spacing.3'),
-						},
-						"h3": {
-							marginTop: theme('spacing.6'),
-							marginBottom: theme('spacing.3'),
-						},
-					},
-				},
-			}),
-		},
-	},
-	plugins: [require('@tailwindcss/typography')],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#8B1717",
+      },
+      animation: {
+        "fade-in-up": "fade-in-up 0.3s ease-in forwards",
+        "fade-in": "fade-in 0.3s ease-in forwards",
+      },
+      screens: {
+        notouch: { raw: "(hover: hover)" },
+        "2xl": "1440px",
+      },
+      keyframes: {
+        "fade-in-up": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(5rem)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "fade-in": {
+          "0%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
+      },
+      fontFamily: {
+        sans: ["Monterrat", "sans-serif"],
+        serif: ["Playfair Display", "serif"],
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            "--tw-prose-links": theme("colors.brand"),
+            fontFamily: theme("fontFamily.sans").join(","),
+            color: theme("colors.gray.700"),
+            maxWidth: "100%",
+            width: "70ch",
+            a: {
+              textDecoration: "none",
+              underlineOffset: "0.5em",
+              color: theme("colors.brand"),
+              "&:hover": {
+                textDecoration: "underline",
+              },
+            },
+            "h1,h2,h3,h4": {
+              fontFamily: theme("fontFamily.serif").join(","),
+              fontWeight: "600",
+            },
+            h1: {
+              fontSize: theme("fontSize.4xl"),
+            },
+            h2: {
+              fontSize: theme("fontSize.4xl"),
+              marginTop: theme("spacing.10"),
+              marginBottom: theme("spacing.3"),
+            },
+            h3: {
+              fontSize: theme("fontSize.2xl"),
+            },
+            strong: {
+              fontWeight: "600",
+            },
+          },
+        },
+        lg: {
+          css: {
+            h1: {},
+            h2: {
+              fontSize: theme("fontSize.5xl"),
+              marginTop: theme("spacing.10"),
+              marginBottom: theme("spacing.3"),
+            },
+            h3: {
+              marginTop: theme("spacing.6"),
+              marginBottom: theme("spacing.3"),
+            },
+          },
+        },
+      }),
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
